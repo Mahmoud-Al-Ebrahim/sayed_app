2a777ea9d3de8d2a885eda90ccd389c359ef414b
Mahmoud AlEbrahim <ma453935@gmail.com>
2026-06-23 17:56:04 +0300

    fix api tokens for providers

--- src/providers/index.js
+++ src/providers/index.js
@@ -5,22 +5,15 @@ import { TempoClient } from './tempo.client.js';
 import { ShehabiClient } from './shehabi.client.js';
 import { ProviderError } from './base.client.js';
 
-export function getProviderApiToken(providerDoc) {
-  if (!providerDoc.credentials) {
-    throw new ProviderError(msg.PROVIDER_NO_TOKEN(providerDoc.name));
-  }
-  return decrypt(providerDoc.credentials);
-}
 
 export function createProviderClient(providerDoc) {
-  const apiToken = getProviderApiToken(providerDoc);
   const baseUrl = providerDoc.websiteUrl || undefined;
 
   switch (providerDoc.providerType) {
     case PROVIDER_TYPES.TEMPO:
-      return new TempoClient({ apiToken, baseUrl });
+      return new TempoClient();
     case PROVIDER_TYPES.SHEHABI:
-      return new ShehabiClient({ apiToken, baseUrl });
+      return new ShehabiClient();
     default:
       throw new ProviderError(msg.PROVIDER_UNSUPPORTED_TYPE(providerDoc.providerType));
   }
